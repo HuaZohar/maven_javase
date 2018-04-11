d4a80bc17b68845de6d7359a9e731dc0fb6974ac
zzhuaq <358824891@qq.com>
2018-04-11 09:48:43 +0800

    init

---
+++ src/testMain.java
@@ -0,0 +1,11 @@
+public class testMain {
+    public static void main(String[] args) {
+        System.out.println("fasfdadjfkasjfdljsakdfjakdfasjdfadf");
+        int age = getAge();
+        System.out.println(age);
+    }
+
+    public static Integer getAge() {
+        return 12;
+    }
+}
